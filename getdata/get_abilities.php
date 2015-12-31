fe521ba0d3ebcdea6f9d689d18bdc71bbfc4be9f
Pioneer11X <sravan@mysmartprice.com>
2015-12-31 19:42:27 +0000

    Testing out new Abilities Strings

--- getdata/get_abilities.php
+++ getdata/get_abilities.php
@@ -11,7 +11,8 @@ foreach ( $jsonArray as $heroName=>$data ){
 }
 
 $jsonString = json_encode($abilitiesArray);
-$jsonString = str_replace(array("{","}"),array("[","]"),$jsonString);
+$jsonString = str_replace(array("{","}","\\"),array("[","]",""),$jsonString);
+
 
 file_put_contents($newFile,$jsonString);
 
