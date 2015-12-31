9d7eb1f810eb545465a2509276a5f15a80d4e771
Pioneer11X <sravan@mysmartprice.com>
2015-12-31 19:16:05 +0000

    Added a json file for ability description

---
+++ getdata/get_abilities.php
@@ -0,0 +1,19 @@
+<?php
+
+$fp = file_get_contents("../resources/heroes.json","r");
+$newFile = "../resources/abilities.json";
+
+$jsonArray = json_decode($fp,true);
+$abilitiesArray = array();
+
+foreach ( $jsonArray as $heroName=>$data ){
+	$abilitiesArray[$heroName] = $data["abilities"];
+}
+
+$jsonString = json_encode($abilitiesArray);
+
+file_put_contents($newFile,$jsonString);
+
+//echo $jsonArray["Axe"]["bio"];
+
+?>
